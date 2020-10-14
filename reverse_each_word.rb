e3096cf23bdd018b9257dd097792e06ab6f6dc87
Avelon Pang <avelonpang@gmail.com>
2020-10-14 17:35:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,8 +15,13 @@ def reverse_each_word(string)
 
 #string = string.split('')
 #reversed = []
-string.each {|i| reversed.push(i.reverse)}
+#string.each {|i| reversed.push(i.reverse)}
 #string.each do |i| reversed_word = i.reverse 
+#reversed.push(reversed_word)
+#return reversed.join('')
+
+string = string.split('')
+string.each do |i| reversed_word = i.reverse 
 reversed.push(reversed_word)
 return reversed.join('')
 
