3e8b3152443509ee818153bb4ed2ccc74b92eae4
Avelon Pang <avelonpang@gmail.com>
2020-10-14 17:13:35 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(string) 
+#string.each do |string|
+#puts string.reverse()
+#string.reverse() #reverses the words and letters in the String
+
+#can't use .each on string, split into an array:
+#string.split().each do |string1|
+#string1.reverse()
+
+#array = string.split(' ')
+#array.reverse_each {|x| print x, " " }
+#string.split(' ').reverse_each {|x| print x, "" }
+
+#string.each {|i| reversed.unshift(i)}
+
+string = string.split('')
+reversed = []
+string.each {|i| reversed.unshift(i)}
+return reversed.join('')
+
+end 
