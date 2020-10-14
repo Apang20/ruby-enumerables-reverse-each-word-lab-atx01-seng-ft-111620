fcb5b00d27acaec15b688ffcce4c4f7b0e429331
Avelon Pang <avelonpang@gmail.com>
2020-10-14 18:32:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -39,7 +39,7 @@ end
 # end
 
 def reverse_each_word(string) #collect returns new arr 
-  string_array = string.split(' ')
+string_array = string.split(' ')  
   string_array.collect do |i|
   	i.reverse! #bang secures the reverse 
   end.join(' ')
