ad02145aa0fc129816d6576195cc4c9d11dd7e3e
Avelon Pang <avelonpang@gmail.com>
2020-10-14 17:29:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,7 +15,7 @@ def reverse_each_word(string)
 
 string = string.split('')
 reversed = []
-string.each {|i| reversed.unshift(i)}
+string.each {|i| reversed.push(i.reverse)}
 return reversed.join('')
 
 end 
